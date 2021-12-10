fbd027a06fb6f3be92d930501240a6b5685a97a2
Flandre3569 <1332726596@qq.com>
2021-12-10 19:45:45 +0800

    项目初始化

---
+++ src/main/java/org/edu/example/App.java
@@ -0,0 +1,13 @@
+package org.edu.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
