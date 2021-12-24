66f744d5f86f11cc2cf4c2d56d43f4057a7539df
Flandre3569 <1332726596@qq.com>
2021-12-24 17:35:39 +0800

    上传pdf功能

--- bms/src/main/java/com/example/bms/controller/UserHandler.java
+++ bms/src/main/java/com/example/bms/controller/UserHandler.java
@@ -5,17 +5,22 @@ import com.example.bms.aop.LogAnnotation;
 import com.example.bms.entity.User;
 import com.example.bms.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserHandler {
     @Autowired
     private UserMapper userMapper;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("/yyyy/MM/dd");
     @GetMapping("/selectAll")
     @LogAnnotation(module="用户",operator="获取所有用户")
     public List<User> findAll() {
@@ -25,5 +30,34 @@ public class UserHandler {
     @GetMapping("/countUser")
     public Integer countUser() {
         return userMapper.countUser();
+    }
+
+    @PostMapping("/upload")
+    public Map<String, Object> fileupload(@RequestBody MultipartFile file, HttpServletRequest req) {
+        System.out.println(file);
+        Map<String, Object> result = new HashMap<>();
+        String originName  = file.getOriginalFilename();
+        if(!originName.endsWith(".pdf")) {
+            result.put("status","error");
+            result.put("msg","文件类型错误");
+            return result;
+        }
+        String format = sdf.format(new Date());
+        String realPath = req.getServletContext().getRealPath("/") + format;
+        File folder = new File(realPath);
+        if(!folder.exists()) {
+            folder.mkdirs();
+        }
+        String newName = UUID.randomUUID().toString() + ".pdf";
+        try {
+            file.transferTo(new File(folder,newName));
+            result.put("status","success");
+            String url = req.getScheme()+ "://" + req.getServerName() + ":" + req.getServerPort() + format + newName;
+            result.put("url",url);
+        } catch (IOException e) {
+            result.put("status","error");
+            result.put("msg",e.getMessage());
+        }
+        return result;
     }
 }
--- bms/src/main/java/com/example/bms/controller/UsersHandler.java
+++ bms/src/main/java/com/example/bms/controller/UsersHandler.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -26,6 +27,8 @@ public class UsersHandler {
     @PostMapping("/addUser")
     @LogAnnotation(module="用户",operator="添加用户")
     public String addUser(@RequestBody Users user) {
+        String md5Password = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        user.setPassword(md5Password);
         Users result = usersRepository.save(user);
         if(result != null) {
             return "success";
